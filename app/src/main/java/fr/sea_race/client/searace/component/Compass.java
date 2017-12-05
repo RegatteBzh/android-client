fe45d3d06e576505454d7a4fa83e09384b6b0014
Cyrille Meichel <cmeichel@free.fr>
2017-12-05 21:51:35 +0100

    create compass component

---
+++ app/src/main/java/fr/sea_race/client/searace/component/Compass.java
@@ -0,0 +1,30 @@
+package fr.sea_race.client.searace.component;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by cyrille on 05/12/17.
+ */
+
+public class Compass extends View {
+
+    public Compass(Context context) {
+        super(context);
+    }
+
+    public Compass(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+
+    }
+
+}
